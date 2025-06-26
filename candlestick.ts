ae74748c70328314747ce1628cbc043c58d96efc
Adlai Arnold <adlai.arnold@primestagetechnology.com>
2025-06-26 10:28:08 -0700

    Refactor candlestick processing logic for enhanced clarity and functionality
    - Removed the unused `getMean` import to streamline dependencies.
    - Introduced new functions `processOverflow`, `processPartial`, and their respective leaf and branch handlers to improve the structure of candlestick processing logic.
    - Enhanced logging for better traceability during sample processing, particularly when handling tier updates and historical candlestick creation.
    - Updated the `processSamples` function to utilize the new overflow and partial processing methods, improving readability and maintainability of the code.
    - Adjusted the `toHistoricalCandlesticks` function to calculate the number of synthetic candlesticks based on distance, ensuring accurate historical data generation.

--- candlestick.ts
+++ candlestick.ts
@@ -12,7 +12,6 @@ import {
   getCloseAt,
   getHigh,
   getLow,
-  getMean,
   getOpen,
   getOpenAt,
   getTimeWeightedMean,
@@ -188,6 +187,7 @@ export function processSamples(
   eternal: Candlestick;
 } {
   const [tier, ...restTiers] = tiers;
+  console.log("processing tier", tier.name);
   const newestSample = R.last(samples);
   const newestSampleCandlestick = toCandlestick(newestSample);
   const oldestSample = R.head(samples);
@@ -201,121 +201,230 @@ export function processSamples(
   const distanceExceedsTierDuration =
     distance > tier.duration.as("milliseconds");
   if (distanceExceedsTierDuration) {
-    // newest sample's time means we should historize "current"
-    const newHistoricalCandlestick = historizeCandlestick(
-      tier.current,
-      tier.duration,
-    );
-
-    const howManyCurrentTierDurationsSinceLastSample = Math.floor(
-      distance / tier.duration.as("milliseconds"),
-    );
-    const historicalCandlesticks = toHistoricalCandlesticks(
-      newHistoricalCandlestick,
-      tier.duration,
-      howManyCurrentTierDurationsSinceLastSample,
-    );
     console.log(
-      "newHistoricalCandlesticks",
-      JSON.stringify(historicalCandlesticks),
+      "distance exceeds tier duration, historizing current:",
+      JSON.stringify(tier.current),
     );
-    if (R.isEmpty(restTiers)) {
-      console.log("leaf node, new bucket");
-      // if the newest sample should result in the current candlestick being historized
-      // and there are no tiers to cascade to
-      // then return this tier with the current candlestick
-      // and set newest sample candlestick as eternal
-      // and set the samples to just the newest sample
-
-      const eternal = reduceCandlesticks([
-        tier.current,
-        newestSampleCandlestick,
-      ]);
-      // console.log(
-      //   `newestSampleCandlestick: ${JSON.stringify(newestSampleCandlestick)}`,
-      // );
-      // console.log("thisTier.current");
-      // renderSmartCandlesticks(
-      //   [thisTier.current],
-      //   thisTier.current.closeAt.diff(thisTier.current.openAt),
-      // );
-      // console.log("newestSampleCandlestick");
-      // renderSmartCandlesticks([newestSampleCandlestick], thisTier.duration);
-      // console.log("eternal");
-      // renderSmartCandlesticks(
-      //   [eternal],
-      //   newestSampleCandlestick.closeAt.diff(
-      //     tiers?.[0]?.history?.[0]?.openAt || Duration.fromMillis(0),
-      //   ).as("seconds"),
-      // );
-      // the current openAt is either the last history's closeAt or, if this is the first sample, that sample's datetime
-      const currentOpenAt = newHistoricalCandlestick?.closeAt ||
-        newestSample.dateTime;
-      const newCurrent = {
-        ...newestSampleCandlestick,
-        openAt: currentOpenAt,
-      };
-      const history = [...historicalCandlesticks]; // todo do we need this? eternal should handle history in this case
-      return {
-        tiers: [{
-          ...tier,
-          history,
-          current: newCurrent,
-        }],
-        eternal,
-      };
-    } else {
-      console.log("branch, new bucket");
-      // if the newest sample should result in this tier's current candlestick being historized
-      // and there are other tiers to cascade to
-      // then recurse into the rest of the tiers
-      // and set the samples to just the newest sample
-      const recursed = processSamples(
-        restTiers as NonEmptyArray<Tier>,
-        samples,
-      );
-      const history = [...tier.history, ...historicalCandlesticks];
-      const newCurrentTier = {
-        ...tier,
-        history,
-        current: newestSampleCandlestick,
-      };
-      return {
-        tiers: [newCurrentTier, ...recursed.tiers],
-        eternal: recursed.eternal,
-      };
-    }
-  } else {
-    console.log("leaf node, no new bucket");
-    const newCurrentCandlestick = reduceCandlesticks([
-      tier.current,
+    // newest sample's time means we should historize "current"
+    return processOverflow(
+      tier,
       newestSampleCandlestick,
-    ]);
-    const newCurrentTier = { ...tier, current: newCurrentCandlestick };
-    if (R.isEmpty(restTiers)) {
-      // if the newest sample should not result in this tier's current candlestick being historized
-      // and there are no other tiers to cascade to
-      // then just update the new current candlestick
-      return {
-        tiers: [newCurrentTier],
-        eternal: newCurrentCandlestick,
-      };
-    } else {
-      console.log("branch, no new bucket");
-      // if the newest sample should not result in this tier's current candlestick being historized
-      // and there are other tiers to cascade to
-      // then just update the new current candlestick and process the rest of the tiers
-      // (which should also just update their current candlesticks)
-      const recursed = processSamples(
-        restTiers as NonEmptyArray<Tier>,
-        samples,
-      );
-      return {
-        tiers: [newCurrentTier, ...recursed.tiers],
-        eternal: recursed.eternal,
-      };
-    }
+      newestSample,
+      distance,
+      restTiers as NonEmptyArray<Tier>,
+      samples,
+    );
+  } else {
+    // if the newest sample should not result in this tier's current candlestick being historized
+    // then just update the new current candlestick
+    return processPartial(
+      tier,
+      newestSampleCandlestick,
+      restTiers as NonEmptyArray<Tier>,
+      samples,
+    );
   }
+}
+
+export function processOverflow(
+  tier: Tier,
+  newestSampleCandlestick: Candlestick,
+  newestSample: Sample,
+  distance: Duration,
+  restTiers: NonEmptyArray<Tier>,
+  samples: NonEmptyArray<Sample>,
+): {
+  tiers: NonEmptyArray<Tier>;
+  eternal: Candlestick;
+} {
+  const newHistoricalCandlestick = historizeCandlestick(
+    tier.current,
+    tier.duration,
+  );
+  const historicalCandlesticks = toHistoricalCandlesticks(
+    newHistoricalCandlestick,
+    tier.duration,
+    distance,
+  );
+  console.log(
+    "newHistoricalCandlesticks",
+    JSON.stringify(historicalCandlesticks),
+  );
+  if (R.isEmpty(restTiers)) {
+    console.log("leaf node, new bucket");
+    // if the newest sample should result in the current candlestick being historized
+    // and there are no tiers to cascade to
+    // then return this tier with the current candlestick
+    // and set newest sample candlestick as eternal
+    // and set the samples to just the newest sample
+    return processOverflowLeaf(
+      tier,
+      newestSampleCandlestick,
+      historicalCandlesticks,
+      newestSample,
+      newHistoricalCandlestick,
+    );
+  } else {
+    console.log("branch, new bucket");
+    // if the newest sample should result in this tier's current candlestick being historized
+    // and there are other tiers to cascade to
+    // then recurse into the rest of the tiers
+    // and set the samples to just the newest sample
+    return processOverflowBranch(
+      tier,
+      restTiers as NonEmptyArray<Tier>,
+      samples,
+      historicalCandlesticks,
+      newestSampleCandlestick,
+    );
+  }
+}
+export function processOverflowLeaf(
+  tier: Tier,
+  newestSampleCandlestick: Candlestick,
+  historicalCandlesticks: NonEmptyArray<Candlestick>,
+  newestSample: Sample,
+  newHistoricalCandlestick: Candlestick,
+): {
+  tiers: NonEmptyArray<Tier>;
+  eternal: Candlestick;
+} {
+  const eternal = reduceCandlesticks([
+    tier.current,
+    newestSampleCandlestick,
+  ]);
+  // console.log(
+  //   `newestSampleCandlestick: ${JSON.stringify(newestSampleCandlestick)}`,
+  // );
+  // console.log("thisTier.current");
+  // renderSmartCandlesticks(
+  //   [thisTier.current],
+  //   thisTier.current.closeAt.diff(thisTier.current.openAt),
+  // );
+  // console.log("newestSampleCandlestick");
+  // renderSmartCandlesticks([newestSampleCandlestick], thisTier.duration);
+  // console.log("eternal");
+  // renderSmartCandlesticks(
+  //   [eternal],
+  //   newestSampleCandlestick.closeAt.diff(
+  //     tiers?.[0]?.history?.[0]?.openAt || Duration.fromMillis(0),
+  //   ).as("seconds"),
+  // );
+  // the current openAt is either the newest history's closeAt or, if this is the first sample, that sample's datetime
+  const currentOpenAt = newHistoricalCandlestick?.closeAt ||
+    newestSample.dateTime;
+  const newCurrent = {
+    ...newestSampleCandlestick,
+    openAt: currentOpenAt,
+  };
+  console.log(
+    `leaf node new bucket: newCurrent: ${JSON.stringify(newCurrent)}`,
+  );
+  const history = [...historicalCandlesticks]; // todo do we need this? eternal should handle history in this case
+  return {
+    tiers: [{
+      ...tier,
+      history,
+      current: newCurrent,
+    }],
+    eternal,
+  };
+}
+
+export function processOverflowBranch(
+  tier: Tier,
+  restTiers: NonEmptyArray<Tier>,
+  samples: NonEmptyArray<Sample>,
+  historicalCandlesticks: NonEmptyArray<Candlestick>,
+  newestSampleCandlestick: Candlestick,
+): {
+  tiers: NonEmptyArray<Tier>;
+  eternal: Candlestick;
+} {
+  const recursed = processSamples(
+    restTiers as NonEmptyArray<Tier>,
+    samples,
+  );
+  const history = [...tier.history, ...historicalCandlesticks];
+  const newCurrentTier = {
+    ...tier,
+    history,
+    current: newestSampleCandlestick,
+  };
+  return {
+    tiers: [newCurrentTier, ...recursed.tiers],
+    eternal: recursed.eternal,
+  };
+}
+
+export function processPartial(
+  tier: Tier,
+  newestSampleCandlestick: Candlestick,
+  restTiers: NonEmptyArray<Tier>,
+  samples: NonEmptyArray<Sample>,
+): {
+  tiers: NonEmptyArray<Tier>;
+  eternal: Candlestick;
+} {
+  const newCurrentTier = addSampleCandlestickToCurrent(
+    tier,
+    newestSampleCandlestick,
+  );
+  if (R.isEmpty(restTiers)) {
+    // if there are no other tiers to cascade to
+    // then just return the new current tier and update eternal
+    return processPartialLeaf(newCurrentTier);
+  } else {
+    // if there are other tiers to cascade to
+    // then just update the new current candlestick and recurse into the other tiers
+    // (which will update their current candlesticks)
+    return processPartialBranch(
+      newCurrentTier,
+      restTiers as NonEmptyArray<Tier>,
+      samples,
+    );
+  }
+}
+
+export function addSampleCandlestickToCurrent(
+  tier: Tier,
+  newestSampleCandlestick: Candlestick,
+) {
+  const newCurrentCandlestick = reduceCandlesticks([
+    tier.current,
+    newestSampleCandlestick,
+  ]);
+  return { ...tier, current: newCurrentCandlestick };
+}
+
+export function processPartialLeaf(
+  tier: Tier,
+): {
+  tiers: NonEmptyArray<Tier>;
+  eternal: Candlestick;
+} {
+  return {
+    tiers: [tier],
+    eternal: tier.current,
+  };
+}
+
+export function processPartialBranch(
+  currentTier: Tier,
+  tiers: NonEmptyArray<Tier>,
+  samples: NonEmptyArray<Sample>,
+): {
+  tiers: NonEmptyArray<Tier>;
+  eternal: Candlestick;
+} {
+  const recursed = processSamples(
+    tiers,
+    samples,
+  );
+  return {
+    tiers: [currentTier, ...recursed.tiers],
+    eternal: recursed.eternal,
+  };
 }
 
 export function getDistance(
@@ -385,8 +494,11 @@ function renderSamples(samples: R.NonEmptyArray<Sample>): void {
 export function toHistoricalCandlesticks(
   candlestick: Candlestick,
   duration: Duration,
-  numToGenerate: number,
+  distance: Duration,
 ): R.NonEmptyArray<Candlestick> {
+  const numToGenerate = Math.floor(
+    distance / duration.as("milliseconds"),
+  );
   const syntheticCandlesticks = (numToGenerate > 1)
     ? R.range(
       0,
